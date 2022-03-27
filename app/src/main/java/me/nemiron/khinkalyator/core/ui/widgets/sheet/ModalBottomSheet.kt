a9a82397909950b5f67201bdaf521466d32b602b
Pavel Aleksandrov <pav.aleksandrov@x5.ru>
2022-03-27 21:53:25 +0300

    Import ModalBottomSheet composable for wrapping Material bottom-sheet

---
+++ app/src/main/java/me/nemiron/khinkalyator/core/ui/widgets/sheet/ModalBottomSheet.kt
@@ -0,0 +1,109 @@
+package me.nemiron.khinkalyator.core.ui.widgets.sheet
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ModalBottomSheetDefaults
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import me.aartikov.sesame.dialog.DialogControl
+import me.nemiron.khinkalyator.core.ui.theme.appShapes
+
+/*
+* ModalBottomSheetDialogLayout требуется для отображения BottomSheet на экране.
+* Так же данная функция берет на себя ответственность за исправление бага
+* во время анимации закрытия клавиатуры. По этой причине здесь имеется различные
+* проверки на видимость modalBottomSheetState.
+* Поэтому в случае открытой клавиатуры и закрытого modalBottomSheetState необходимо:
+* цвет фона сделать прозрачным, установить Elevation в 0, скрыть контент.
+* WARNING: sheetContent не может быть пустым, по этой причине используется Spacer
+* */
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun <T : Any, F : Any> ModalBottomSheet(
+    dialogControl: DialogControl<T, F>,
+    modifier: Modifier = Modifier,
+    // FIXME: is it good place for such logic?
+    dropFocusAfterDismiss: Boolean = true,
+    sheetContent: @Composable (T) -> Unit
+) {
+    val state by dialogControl.stateFlow.collectAsState()
+    val modalBottomSheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        confirmStateChange = { it != ModalBottomSheetValue.HalfExpanded }
+    )
+    val focusManager = LocalFocusManager.current
+
+    if (state is DialogControl.State.Shown) {
+        if (modalBottomSheetState.currentValue != ModalBottomSheetValue.Expanded) {
+            LaunchedEffect(modalBottomSheetState) {
+                modalBottomSheetState.show()
+            }
+        }
+    } else {
+        if (modalBottomSheetState.currentValue != ModalBottomSheetValue.Hidden) {
+            LaunchedEffect(modalBottomSheetState) {
+                modalBottomSheetState.hide()
+            }
+        }
+    }
+
+    if (modalBottomSheetState.currentValue != ModalBottomSheetValue.Hidden) {
+        DisposableEffect(Unit) {
+            onDispose {
+                if (dropFocusAfterDismiss) {
+                    focusManager.clearFocus()
+                }
+                dialogControl.dismiss()
+            }
+        }
+    }
+
+    val sheetElevation = if (modalBottomSheetState.isVisible) {
+        ModalBottomSheetDefaults.Elevation
+    } else {
+        0.dp
+    }
+
+    val sheetBackgroundColor = if (modalBottomSheetState.isVisible) {
+        MaterialTheme.colors.background
+    } else {
+        Color.Transparent
+    }
+
+    ModalBottomSheetLayout(
+        modifier = modifier,
+        sheetState = modalBottomSheetState,
+        sheetShape = MaterialTheme.appShapes.sheet.copy(
+            bottomStart = CornerSize(0.dp),
+            bottomEnd = CornerSize(0.dp)
+        ),
+        sheetElevation = sheetElevation,
+        sheetBackgroundColor = sheetBackgroundColor,
+        sheetContent = {
+            val data = dialogControl.dataOrNull
+            if (data != null && modalBottomSheetState.isVisible) {
+                sheetContent(data)
+            } else {
+                Spacer(Modifier.height(1.dp))
+            }
+        },
+        content = {}
+    )
+}
+
+val <T : Any, R : Any> DialogControl<T, R>.dataOrNull: T?
+    get() = (this.stateFlow.value as? DialogControl.State.Shown)?.data
